638a0b79ff4b05d172f9d02ecf8fc16f6be7f465
aer <aer@macaer.local>
2025-02-05 12:42:08 -0500

    Creating folder

---
+++ tf/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "example" {
+  filename = "example.txt"
+  content  = "Hello, friend."
+}
+
