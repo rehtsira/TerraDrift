127e727bfe8885e359211305c5503a5a7270a916
aer <aer@macaer.local>
2025-03-24 10:59:09 -0400

    Updating description

--- modules/iam/admins.tf
+++ modules/iam/admins.tf
@@ -1,5 +1,5 @@
 resource "local_file" "admins" {
   filename = "local_admins.txt"
-  content  = "This is from the iam module creating admins.txt file"
+  content  = "This is from the iam module creating local_admins.txt file"
 }
 
